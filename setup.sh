c7fe332d961ccf9eacf154efadd1d8216cefb411
Aniruddha Prithul <aprithul@gmail.com>
2022-04-04 19:23:21 -0700

    setup file to auto configure sdl and glew
    * Installs sdl2 and glew using homebrew.
    * Sets up the symlinks.

---
+++ setup.sh
@@ -0,0 +1,6 @@
+brew install SDL2
+brew install glew
+ln -s /opt/homebrew/include/SDL2 include/SDL2
+ln -s /opt/homebrew/include/GL include/GL
+ln -s /opt/homebrew/lib/ lib/SDL2
+ln -s /opt/homebrew/lib/ lib/GL
